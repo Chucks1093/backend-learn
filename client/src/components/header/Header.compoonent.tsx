61822e09aa247d93192a3132aa4d67e9cadd6f20
Sebastian Anioke <aniokechukwudi7@gmail.com>
2023-11-09 17:32:41 +0100

    routing works perfectly after deploy to server

--- client/src/components/header/Header.compoonent.tsx
+++ client/src/components/header/Header.compoonent.tsx
@@ -5,7 +5,7 @@ import { onClickSound } from "../../utils/sounds";
 function Header() {
   return (
     <div className="header">
-       <h3>NASA Mission control 🚀</h3>
+       <h3>NASA Mission control 🪐</h3>
        <div>
           <Link onClick={onClickSound} to="/">Launches</Link>
           <Link onClick={onClickSound} to="/upcoming">Upcoming</Link>
